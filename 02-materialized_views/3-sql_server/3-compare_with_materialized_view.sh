c003e7263923217ccd2836d03b36eac1d3133edf
Rafa Gómez <rgomezcasas@gmail.com>
2024-03-04 16:57:23 +0100

    feat(2-3): add base

---
+++ 02-materialized_views/3-sql_server/3-compare_with_materialized_view.sh
@@ -0,0 +1,1 @@
+hyperfine --warmup 3 'bash queries/table_select.sh' 'bash queries/materialized_view_count_select.sh'
---
+++ 02-materialized_views/3-sql_server/5-compare_with_materialized_view_latest_tweets.sh
@@ -0,0 +1,1 @@
+hyperfine --warmup 3 'bash queries/table_select.sh' 'bash queries/materialized_view_latest_tweets_select.sh'
